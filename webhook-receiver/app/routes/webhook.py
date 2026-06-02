341410a9ae24865e9b8df4b91d77fbc9bbd4dd25
veergandhii <53veergandhi35@gmail.com>
2026-06-02 12:03:08 +0530

    INTEGRATING RABBITMQ AND DOCKER COMPOSE

--- webhook-receiver/app/routes/webhook.py
+++ webhook-receiver/app/routes/webhook.py
@@ -1,7 +1,14 @@
-from fastapi import  APIRouter,Request 
-router=APIRouter()
+from fastapi import APIRouter, Request
+from app.publisher import publish_message
+
+router = APIRouter()
+
 @router.post("/webhook")
-async def handle_webhook(request:Request):
-    payload=await request.json()
-    print("Received webhook payload:",payload)
-    return{"status":"received"}
+async def handle_webhook(request: Request):
+    payload = await request.json()
+    try:
+        await publish_message(payload)
+        print("Received webhook payload:", payload)
+    except Exception as e:
+        print(f"Publisher failed: {e}")  # this will show what's actually breaking
+    return {"status": "received"}
